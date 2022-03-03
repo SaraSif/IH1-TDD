9bf2310af5db2761dc4903a5936c5a9887cee604
SaraSif <89972096+SaraSif@users.noreply.github.com>
2022-03-03 11:19:27 +0000

    step 2. implement Buzz, divisable by 5

--- sample.py
+++ sample.py
@@ -1,3 +1,5 @@
 def fizzbuzz(integer):
     if integer % 5 == 0 and integer % 3 == 0:
         return "FizzBuzz"
+    if integer % 5 == 0 and integer % 3 != 0:
+        return "Buzz"
--- test_sample.py
+++ test_sample.py
@@ -8,3 +8,12 @@ def test_fizzbuzz_with_divisable_2():
 
 def test_fizzbuzz_with_divisable_3():
     assert fizzbuzz(-60) == "FizzBuzz"
+
+def test_buzz_with_divisable_1():
+    assert fizzbuzz(5) == "Buzz"
+
+def test_buzz_with_divisable_1():
+    assert fizzbuzz(10) == "Buzz"
+
+def test_buzz_with_divisable_1():
+    assert fizzbuzz(-20) == "Buzz"
